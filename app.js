fd72e9af53ef7ef8100f38bb89201d02559b6f12
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-03-23 16:58:49 -0700

    finished progect

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let index
-const formValues = localStorage.getItem('formValues')
+// let index
+// const formValues = localStorage.getItem('formValues')
+const formValues = []
+var inputs = document.querySelectorAll('input:not([type="submit"])')
+let message = document.getElementById('message')
+let span = document.querySelectorAll('span')
 
 
 const form = document.querySelector('form')
@@ -7,25 +11,33 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+
+  // the checkInput function only gets the chance to run if you dont use "required" or pattern in the html
+  if (!checkInput()) return
+
   // var formValues = storedData ? storedData : []
-  // var formValues = []
 
   let name = document.getElementById('name').value
   let email = document.getElementById('email').value
-  let message = document.getElementById('message').children[1].value 
   console.log(formValues)
 
-  formValues.push({Name: name, Email: email, Message: message})
-  localStorage.setItem('formValues', JSON.stringify(formValues))
+  formValues.push({Name: name, Email: email, Message: message.children[1].value})
+  // localStorage.setItem('formValues', JSON.stringify(formValues))
   console.table(formValues)
-  console.log(storedData.data)
-  // reset()
+  reset()
 })
 
+// Fill span tag of empty input with text 
+function checkInput() {
+  let valid = true
+  for (let i = 0; i < inputs.length; i++) {
+    span[i].innerText = inputs[i].value == "" ? "Please Fill In Input" : ""
+    if (inputs[i] == "") valid = false 
+  }
+  return valid
+}
 
 function reset() {
-  let inputs = document.querySelectorAll('input:not([type="submit"])')
-  let message = document.getElementById('message')
   inputs.forEach(input => input.value = "")
   message.children[1].value = ""
 }
